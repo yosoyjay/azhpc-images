f806b53313f32bf471bf766aafd59233642e9463
Paul Edwards <paul.edwards@microsoft.com>
2023-12-05 11:28:14 +0000

    updated checksum

--- debian/debian-11.x/debian-11.8/install_mpis.sh
+++ debian/debian-11.x/debian-11.8/install_mpis.sh
@@ -2,4 +2,4 @@
 set -ex
 
 # Install common MPIs for Debian
-$DEBIAN_COMMON_DIR/install_mpis.sh "addda11a710c52268b7a725c13d9cc54c93deddf259c87d8547ad3c0422b87e1"
+$DEBIAN_COMMON_DIR/install_mpis.sh "e1302dd0de260ed4b128b5f90245ea941f0eace43b3cc7ac8891f091a2b50ee1"
