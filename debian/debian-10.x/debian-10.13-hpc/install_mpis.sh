dabbcb967b5e35b58bcf7e439dbd9dcd17564d27
Paul Edwards <paul.edwards@microsoft.com>
2023-12-05 12:56:01 +0000

    try again

--- debian/debian-10.x/debian-10.13-hpc/install_mpis.sh
+++ debian/debian-10.x/debian-10.13-hpc/install_mpis.sh
@@ -2,4 +2,4 @@
 set -ex
 
 # Install common MPIs for Ubuntu 18
-../../common/install_mpis.sh "e1302dd0de260ed4b128b5f90245ea941f0eace43b3cc7ac8891f091a2b50ee1"
+../../common/install_mpis.sh "f1ef2cd9190cb42b099fec926b605fccbb498fbd629c9385c1f432a1e8407c71"
