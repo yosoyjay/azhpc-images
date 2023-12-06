8fec9f7aca7a7b64124e84c57feec13029ed83fc
Paul Edwards <paul.edwards@microsoft.com>
2023-12-06 09:44:39 +0000

    fixing DCGM install

--- debian/common/install_dcgm.sh
+++ debian/common/install_dcgm.sh
@@ -4,9 +4,14 @@ set -ex
 # Install DCGM
 # Reference: https://developer.nvidia.com/dcgm#Downloads
 # the repo is already added during nvidia/ cuda installations
-DCGM_VERSION=3.1.8
-apt-get install -y datacenter-gpu-manager=1:${DCGM_VERSION}
-$COMMON_DIR/write_component_version.sh "DCGM" ${DCGM_VERSION}
+#DCGM_VERSION=3.1.8
+#apt-get install -y datacenter-gpu-manager=1:${DCGM_VERSION}
+#$COMMON_DIR/write_component_version.sh "DCGM" ${DCGM_VERSION}
+
+# hardcoding the version for now
+# v3.1.8 is not available for debian 10 in the nvidia repo
+wget https://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/datacenter-gpu-manager_3.1.8_amd64.deb
+apt install -y ./datacenter-gpu-manager_3.1.8_amd64.deb
 
 # Enable the dcgm service
 systemctl --now enable nvidia-dcgm
