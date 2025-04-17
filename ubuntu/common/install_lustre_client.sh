2edd2ca9a3877baa2916303dcef9ac4ef3d17f12
Jesse Lopez <yosoyjay@gmail.com>
2025-04-17 15:42:54 -0700

    [wip] remove uname -r from lustre client package

--- ubuntu/common/install_lustre_client.sh
+++ ubuntu/common/install_lustre_client.sh
@@ -10,7 +10,9 @@ LUSTRE_VERSION=$(jq -r '.version' <<< $lustre_metadata)
 source $UBUNTU_COMMON_DIR/setup_lustre_repo.sh
 
 apt-get update
-apt-get install -y amlfs-lustre-client-${LUSTRE_VERSION}=$(uname -r)
+# Version not available in repo
+#apt-get install -y amlfs-lustre-client-${LUSTRE_VERSION}=$(uname -r)
+apt-get install -y amlfs-lustre-client-${LUSTRE_VERSION}
 apt-mark hold amlfs-lustre-client-${LUSTRE_VERSION}
 
 $COMMON_DIR/write_component_version.sh "LUSTRE" ${LUSTRE_VERSION}
